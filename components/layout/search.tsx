6118c02663193d83772c20d1b3e9de872affe881
Erik D. <62072464+erikd256@users.noreply.github.com>
2025-07-20 20:24:33 +0200

    simplify search widget

--- components/layout/search.tsx
+++ components/layout/search.tsx
@@ -4,9 +4,9 @@ import { HiSearch } from "react-icons/hi";
 
 export const Search = () => {
     return (
-    <form method="GET" action={"/q"}>
+    <form method="GET" action="/q">
         <div className="p-0 h-11 absolute right-4 text-derdekleur rounded-full border-2 border-vierdekleur flex">
-            <input className="rounded-l-full m-0 h-10 border-derdekleur font-bold bg-basiskleur border-0 placeholder-current p-2 text-sm flex-initial inline-block" placeholder="Zoeken ..." id="search" name="k"></input>
+            <input className="rounded-l-full m-0 h-10 border-derdekleur font-bold bg-basiskleur border-0 placeholder-current p-2 text-sm flex-initial inline-block" placeholder="Zoeken ..." id="search" name="k"/>
             <button type="submit" className="rounded-r-full text-center m-0 h-10 w-10 border-derdekleur font-bold bg-derdekleur text-basiskleur border-1 p-2 text-sm inline-block"><HiSearch/></button>
         </div>
     </form>
