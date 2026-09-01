dbdad9fcf92ef3cb6704e3a7724c27fd3d62cfe9
St. Franciscusparochie Heverlee <webservices@stfranciscus-heverlee.org>
2026-09-01 22:20:31 +0200

    update search component

--- components/layout/search.tsx
+++ components/layout/search.tsx
@@ -11,17 +11,21 @@ function Hit({ hit }) {
       <h1>
         <Highlight attribute="name" hit={hit} />
       </h1>
-      <p>${hit.content}</p>
+      <h1>{hit.title}</h1>
+      <p>{hit.description}</p>
     </article>
   );
 }
 export const Search = () => {
     return (
-        <div className="p-0 h-11 absolute right-4 text-derdekleur rounded-full border-2 border-vierdekleur flex">
-    <InstantSearch indexName="YourIndexName" searchClient={searchClient}>
+        <><div className="p-0 h-11 absolute right-4 text-derdekleur rounded-full border-2 border-vierdekleur flex">
+    <InstantSearch indexName="netlify_f065dc5a-6c9b-4e3c-82b8-a3f548245d4a_main_all" searchClient={searchClient}>
       <SearchBox className="rounded-full m-0 h-10 border-derdekleur font-bold bg-basiskleur border-0 placeholder-current p-2 text-sm flex-initial inline-block"/>
       {/* other widgets */}
-        <Hits hitComponent={Hit} />
-    </InstantSearch></div>
+       <div  className="fixed top-[25%] bg-basiskleur w-[75%] h-[50%] left-[12.5%] overflow-auto"><Hits hitComponent={Hit} /></div>
+    </InstantSearch>
+   </div>
+    </>
+
     )
 }
